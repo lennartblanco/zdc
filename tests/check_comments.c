ecba02c9070e8aafb1907ee7f7f4febbecb5d1f2
Lennart Blanco <cokebuttle@gmail.com>
2009-04-19 12:30:29 +0200

    * added C stubs for comments function test

---
+++ tests/check_comments.c
@@ -0,0 +1,46 @@
+#include "check_utils.h"
+
+/*---------------------------------------------------------------------------*
+ *          wrappers to call test function with D calling convention         *
+ *---------------------------------------------------------------------------*/
+
+bool
+call_dummy1(int in)
+{
+   bool res;
+
+   asm ("    call dummy1\n"
+        : "=a"(res)
+        : "a"(in));
+
+   return res;
+}
+
+int
+call_dummy2()
+{
+   int res;
+
+   asm ("    call dummy2\n"
+        : "=a"(res)
+        : );
+
+   return res;
+}
+
+/*---------------------------------------------------------------------------*
+ *                              run tests                                    *
+ *---------------------------------------------------------------------------*/
+int
+main()
+{
+    /* dummy1() tests */
+    check_bool("dummy1(true)", call_dummy1(true), true);
+    check_bool("dummy1(false)", call_dummy1(false), false);
+
+    /* dummy2() test */
+    check_int("dummy2()", call_dummy2(), 3);
+
+    check_exit();
+}
+
