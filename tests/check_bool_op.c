6f8ab135fea66d117577a2287e6461ec8d1677c5
Lennart Blanco <cokebuttle@gmail.com>
2009-07-27 15:10:35 +0200

    * added some x86 function tests

--- tests/check_bool_op.c
+++ tests/check_bool_op.c
@@ -42,7 +42,95 @@ call_invert(bool in)
    return res;
 }
 
+int
+call_is_equal(int arg1, int arg2)
+{
+   int res;
 
+   asm ("    pushl %[arg1]\n"
+        "    movl %[arg2],%%eax\n"
+        "    call is_equal\n"
+        : "=a"(res)
+        : [arg1]"m"(arg1),
+          [arg2]"m"(arg2));
+
+   return res;
+}
+
+int
+call_not_equal(int arg1, int arg2)
+{
+   int res;
+
+   asm ("    pushl %[arg1]\n"
+        "    movl %[arg2],%%eax\n"
+        "    call not_equal\n"
+        : "=a"(res)
+        : [arg1]"m"(arg1),
+          [arg2]"m"(arg2));
+
+   return res;
+}
+
+int
+call_is_less_or_equal(int arg1, int arg2)
+{
+   int res;
+
+   asm ("    pushl %[arg1]\n"
+        "    movl %[arg2],%%eax\n"
+        "    call is_less_or_equal\n"
+        : "=a"(res)
+        : [arg1]"m"(arg1),
+          [arg2]"m"(arg2));
+
+   return res;
+}
+
+int
+call_is_greater_or_equal(int arg1, int arg2)
+{
+   int res;
+
+   asm ("    pushl %[arg1]\n"
+        "    movl %[arg2],%%eax\n"
+        "    call is_greater_or_equal\n"
+        : "=a"(res)
+        : [arg1]"m"(arg1),
+          [arg2]"m"(arg2));
+
+   return res;
+}
+
+int
+call_is_greater(int arg1, int arg2)
+{
+   int res;
+
+   asm ("    pushl %[arg1]\n"
+        "    movl %[arg2],%%eax\n"
+        "    call is_greater\n"
+        : "=a"(res)
+        : [arg1]"m"(arg1),
+          [arg2]"m"(arg2));
+
+   return res;
+}
+
+int
+call_is_less(int arg1, int arg2)
+{
+   int res;
+
+   asm ("    pushl %[arg1]\n"
+        "    movl %[arg2],%%eax\n"
+        "    call is_less\n"
+        : "=a"(res)
+        : [arg1]"m"(arg1),
+          [arg2]"m"(arg2));
+
+   return res;
+}
 
 /*---------------------------------------------------------------------------*
  *                              run tests                                    *
@@ -60,17 +148,40 @@ main()
     check_bool("invert(true)", call_invert(true), false);
     check_bool("invert(false)", call_invert(false), true);
 
+    /* is_equal() tests */
+    check_bool("is_equal(20, 45)", call_is_equal(20, 45), false);
+    check_bool("is_equal(983, 983)", call_is_equal(983, 983), true);
+
+    /* not_equal() tests */
+    check_bool("not_equal(57, -35)", call_not_equal(57, -35), true);
+    check_bool("not_equal(0, 0)", call_not_equal(0, 0), false);
+
+    /* is_less_or_equal() tests */
+    check_bool("is_less_or_equal(1, 2)", call_is_less_or_equal(1, 2), true);
+    check_bool("is_less_or_equal(2, 2)", call_is_less_or_equal(2, 2), true);
+    check_bool("is_less_or_equal(1, -2)", call_is_less_or_equal(1, -2), false);
+
+    /* is_greater_or_equal() tests */
+    check_bool("is_greater_or_equal(1, 2)", call_is_greater_or_equal(1, 2), false);
+    check_bool("is_greater_or_equal(2, 2)", call_is_greater_or_equal(2, 2), true);
+    check_bool("is_greater_or_equal(1, -2)", call_is_greater_or_equal(1, -2), true);
+
+    /* is_greater() tests */
+    check_bool("is_greater(1, 2)", call_is_greater(1, 2), false);
+    check_bool("is_greater(10, 10)", call_is_greater(10, 10), false);
+    check_bool("is_greater(100, 8)", call_is_greater(100, 8), true);
+
+    /* is_less() tests */
+    check_bool("is_less(1, 2)", call_is_less(1, 2), true);
+    check_bool("is_less(100, 8)", call_is_less(100, 8), false);
+    check_bool("is_less(0, 0)", call_is_less(0, 0), false);
+
 /*
  * todo add test on:
- * is_equal()
- * not_equal() 
- * is_less_or_equal()
- * is_greater_or_equal()
- * is_greater()
+ *
  * greater_tst1()
  * greater_tst2(int x)
  * greater_tst3(int x)
- * is_less(int a, int b)
  * less_tst1()
  * less_tst2(int x)
  * less_tst3(int x)
@@ -78,6 +189,11 @@ main()
  * andor_ops(bool a, bool b, bool c)
  * and_oper(bool left, bool right)
  * or_oper(bool l, bool r)
+ * nested_eq(int arg)
+ *  test cases:
+ * nested_eq(10) == 2
+ * nested_eq(1) == 1
+ *  nested_eq() == 2
  */
    assert(false);
    check_exit();
