c1981a2fe3ae03391bdc8af3ce9db6f6aa3f4f46
Lennart Blanco <cokebuttle@gmail.com>
2009-10-20 07:58:25 +0200

    * fixed typo

--- types.h
+++ types.h
@@ -23,7 +23,7 @@ types_is_literal_0or1(IrExpression *expression);
 /**
  * Perform implicit conversion of an expression to specified
  * data type. The expression is wrapped into cast operation if
- * needed. The expression is of the target type, no modifications are
+ * needed. If the expression is of the target type, no modifications are
  * done.
  *
  * @param target_type the type to convert to
