e7b05cb5eb862371a55b38a5ab37870e53860fe3
Lennart Blanco <cokebuttle@gmail.com>
2009-11-15 12:12:03 +0100

    * moved x86 function call generation code to it's own module

---
+++ x86_func_call.h
@@ -0,0 +1,17 @@
+#ifndef X86_FUNC_CALL_INC_X
+#define X86_FUNC_CALL_INC_X
+
+#include "x86.h"
+#include "ir_function_call.h"
+
+/*---------------------------------------------------------------------------*
+ *                           exported functions                              *
+ *---------------------------------------------------------------------------*/
+
+void
+x86_compile_func_call(x86_comp_params_t *params,
+                      IrFunctionCall *func_call,
+                      sym_table_t *sym_table,
+                      bool retain_return_value);
+
+#endif /* X86_IF_ELSE_INC_X */
