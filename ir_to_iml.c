76a2c16a2cb190ffd2664673cac68b6bb39d2ad9
Lennart Blanco <cokebuttle@gmail.com>
2010-09-28 07:27:38 +0200

    Layout change.

--- ir_to_iml.c
+++ ir_to_iml.c
@@ -12,7 +12,121 @@
 #include <assert.h>
 
 /*---------------------------------------------------------------------------*
+ *                  local functions forward declaration                      *
+ *---------------------------------------------------------------------------*/
+
+static ImlConstant *
+ir_constant_to_iml(IrConstant *constant);
+
+static ImlOperand *
+iml_add_binary_op_eval(IrFunctionDef *function, IrBinaryOperation *bin_op);
+
+static ImlOperand *
+iml_add_func_call_eval(IrFunctionDef *function, IrFunctionCall *func_call);
+
+/*---------------------------------------------------------------------------*
  *                           exported functions                              *
+ *---------------------------------------------------------------------------*/
+
+/**
+ * Add operations to provided function to evaluate the expression.
+ *
+ * @return the variable or constant where the result of expression will be
+ *         stored
+ */
+ImlOperand *
+iml_add_expression_eval(IrFunctionDef *function,
+                        IrExpression *ir_expression)
+{
+    assert(IR_IS_FUNCTION_DEF(function));
+    assert(IR_IS_EXPRESSION(ir_expression));
+
+    if (IR_IS_CONSTANT(ir_expression))
+    {
+        return
+            IML_OPERAND(ir_constant_to_iml(IR_CONSTANT(ir_expression)));
+    }
+    else if (IR_IS_SCALAR(ir_expression))
+    {
+        return
+            IML_OPERAND(
+                ir_variable_get_location(
+                    ir_scalar_get_variable(IR_SCALAR(ir_expression))));
+    }
+    else if (IR_IS_BINARY_OPERATION(ir_expression))
+    {
+        return
+            iml_add_binary_op_eval(function, IR_BINARY_OPERATION(ir_expression));
+    }
+    else if (IR_IS_FUNCTION_CALL(ir_expression))
+    {
+        return iml_add_func_call_eval(function,
+                                      IR_FUNCTION_CALL(ir_expression));
+    }
+    /* unexpected expression type */
+    assert(false);
+}
+
+void
+add_to_func_frame(IrFunctionDef *parent_function,
+                  IrVariable *variable,
+                  bool is_function_parameter)
+{
+    assert(IR_IS_FUNCTION_DEF(parent_function));
+    assert(IR_IS_VARIABLE(variable));
+
+    iml_data_type_t iml_datatype;
+    DtDataType *ir_datatype;
+
+    /* create IML variable of appropriate type */
+    ir_datatype = ir_variable_get_data_type(variable);
+    switch (dt_data_type_get_size(ir_datatype))
+    {
+      case 4:
+        iml_datatype = iml_32b;
+        break;
+      case 2:
+        iml_datatype = iml_16b;
+        break;
+      case 1:
+        iml_datatype = iml_8b;
+        break;
+      default:
+        /* unexpected storage size */
+        assert(FALSE);
+    }
+    ImlVariable *iml_var = iml_variable_new(iml_datatype,
+                                            ir_variable_get_name(variable));
+
+    /* add IML variable to function frame */
+    iml_func_frame_t *frame = ir_function_def_get_frame(parent_function);
+    if (is_function_parameter) {
+        iml_func_frame_add_parameter(frame, iml_var);
+    } else {
+        iml_func_frame_add_local(frame, iml_var);
+    }
+    ir_variable_set_location(variable, iml_var);
+
+    /* if this is a function parameter, then we are done here */
+    if (is_function_parameter) {
+        return;
+    }
+
+    /* */
+    IrExpression *init_exp = ir_variable_get_initializer(variable);
+
+    if (init_exp == NULL)
+    {
+        init_exp = dt_data_type_get_init(ir_variable_get_data_type(variable));
+    }
+
+    ImlOperand *init_val = iml_add_expression_eval(parent_function, init_exp);
+    ir_function_add_operation(parent_function,
+                              iml_operation_new(iml_copy, init_val, iml_var));
+}
+
+/*---------------------------------------------------------------------------*
+ *                             local functions                               *
  *---------------------------------------------------------------------------*/
 
 static ImlConstant *
@@ -161,101 +275,3 @@ iml_add_func_call_eval(IrFunctionDef *function, IrFunctionCall *func_call)
     return IML_OPERAND(res_var);
 
 }
-
-/**
- * Add operations to provided function to evaluate the expression.
- *
- * @return the variable or constant where the result of expression will be 
- *         stored
- */
-ImlOperand *
-iml_add_expression_eval(IrFunctionDef *function,
-                        IrExpression *ir_expression)
-{
-    assert(IR_IS_FUNCTION_DEF(function));
-    assert(IR_IS_EXPRESSION(ir_expression));
-
-    if (IR_IS_CONSTANT(ir_expression))
-    {
-        return
-            IML_OPERAND(ir_constant_to_iml(IR_CONSTANT(ir_expression)));
-    }
-    else if (IR_IS_SCALAR(ir_expression))
-    {
-        return
-            IML_OPERAND(
-                ir_variable_get_location(
-                    ir_scalar_get_variable(IR_SCALAR(ir_expression))));
-    }
-    else if (IR_IS_BINARY_OPERATION(ir_expression))
-    {
-        return
-            iml_add_binary_op_eval(function, IR_BINARY_OPERATION(ir_expression));
-    }
-    else if (IR_IS_FUNCTION_CALL(ir_expression))
-    {
-        return iml_add_func_call_eval(function,
-                                      IR_FUNCTION_CALL(ir_expression));
-    }
-    /* unexpected expression type */
-    assert(false);
-}
-
-void
-add_to_func_frame(IrFunctionDef *parent_function,
-                  IrVariable *variable,
-                  bool is_function_parameter)
-{
-    assert(IR_IS_FUNCTION_DEF(parent_function));
-    assert(IR_IS_VARIABLE(variable));
-
-    iml_data_type_t iml_datatype;
-    DtDataType *ir_datatype;
-
-    /* create IML variable of appropriate type */
-    ir_datatype = ir_variable_get_data_type(variable);
-    switch (dt_data_type_get_size(ir_datatype))
-    {
-      case 4:
-        iml_datatype = iml_32b;
-        break;
-      case 2:
-        iml_datatype = iml_16b;
-        break;
-      case 1:
-        iml_datatype = iml_8b;
-        break;
-      default:
-        /* unexpected storage size */
-        assert(FALSE);
-    }
-    ImlVariable *iml_var = iml_variable_new(iml_datatype,
-                                            ir_variable_get_name(variable));
-
-    /* add IML variable to function frame */
-    iml_func_frame_t *frame = ir_function_def_get_frame(parent_function);
-    if (is_function_parameter) {
-        iml_func_frame_add_parameter(frame, iml_var);
-    } else {
-        iml_func_frame_add_local(frame, iml_var);
-    }
-    ir_variable_set_location(variable, iml_var);
-
-    /* if this is a function parameter, then we are done here */
-    if (is_function_parameter) {
-        return;
-    }
-
-    /* */
-    IrExpression *init_exp = ir_variable_get_initializer(variable);
-
-    if (init_exp == NULL)
-    {
-        init_exp = dt_data_type_get_init(ir_variable_get_data_type(variable));
-    }
-
-    ImlOperand *init_val = iml_add_expression_eval(parent_function, init_exp);
-    ir_function_add_operation(parent_function,
-                              iml_operation_new(iml_copy, init_val, iml_var));
-}
-
