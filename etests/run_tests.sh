ad71a36fb9d92d593070bf267283a043634e2691
Lennart Blanco <cokebuttle@gmail.com>
2010-12-22 07:24:59 +0100

    Split up assigment errors test into two separate tests.
    The compilation will be aborted on 'unknown symbol' errors, thus these
    tests must be run separatly.

--- etests/run_tests.sh
+++ etests/run_tests.sh
@@ -41,7 +41,8 @@ run_test()
 
 run_test "syntx_err" && echo "          ok" || echo "FAILED"
 run_test "array_slice" && echo "        ok" || echo "FAILED"
-#run_test "assigment" && echo "          ok" || echo "FAILED"
+run_test "assigment" && echo "          ok" || echo "FAILED"
+run_test "unknown_symb" && echo "       ok" || echo "FAILED"
 run_test "sym_clash" && echo "          ok" || echo "FAILED"
 #run_test "func_call" && echo "          ok" || echo "FAILED"
 #run_test "types" && echo "              ok" || echo "FAILED"
