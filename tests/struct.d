8ea7756ffe220fe1ba03b210d735b7872b602f5f
Lennart Blanco <cokebuttle@gmail.com>
2011-02-19 15:23:23 +0100

    Added some test code for structs.

--- tests/struct.d
+++ tests/struct.d
@@ -16,6 +16,8 @@ struct list
   uint ref_cntr;
 }
 
+extern (C) list *malloc(uint);
+
 extern (C) uint
 orange_sizeof()
 {
@@ -26,4 +28,24 @@ extern (C) uint
 list_sizeof()
 {
     return list.sizeof;
+}
+
+ptr
+list_get_payload(list *l)
+{
+  return l.payload;
+}
+
+list *
+list_prepend(list *l, ptr payload)
+{
+  list *nl;
+
+  nl = malloc(list.sizeof);
+  nl.next = l;
+  nl.payload = payload;
+  nl.visited = false;
+  nl.ref_cntr = 0;
+
+  return nl;
 }
