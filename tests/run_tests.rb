998ccda55114f3d1639769a7980ac795744464dc
Lennart Blanco <cokebuttle@gmail.com>
2011-05-07 06:41:35 +0200

    tests: added support for building and running x86 local tests

--- tests/run_tests.rb
+++ tests/run_tests.rb
@@ -5,18 +5,54 @@ include REXML
 
 BuildDirRoot = "build"
 
-class TestSet
+class LocalRunner
+  attr_reader :passed
+  attr_reader :failed
+
+  def initialize
+    @passed = 0
+    @failed = 0
+  end
+
+  def run(build_dir, tests)
+
+    orig_dir = Dir.pwd
+    Dir.chdir(build_dir)
+
+    for test in tests
+      print "Running #{test}..."
+      res = `./check_#{test} 2>&1`
+
+      if $? == 0
+        print "[ok]\n"
+        @passed += 1
+      else
+        print "error\n\n", res, "\n"
+        @failed += 1
+      end
+    end
+
+    Dir.chdir(orig_dir)
+  end
+end
+
+class TestTarget
+  attr_reader :name
   def initialize(tests, conf_file)
 
     @tests = tests
+
+    @passed_tests = 0
+    @failed_tests = 0
+    @skiped_tests = 0
 
     # chop off '.conf' and use as name
     @name = conf_file[0..-(".conf".length + 1)]
 
     # get root element
     root = Document.new(File.new(conf_file)).root
-    if root.name != "test-fixture"
-      fail "expected <test-fixture> as root element, got #{root.name}"
+    if root.name != "test-target"
+      fail "expected <test-target> as root element, got #{root.name}"
     end
 
     # get specified arch
@@ -30,13 +66,27 @@ class TestSet
     end
 
     # get runner type
-    @runner_type = root.get_text("runner/type")
+    runner_type = root.get_text("runner/type")
+    if runner_type == nil
+      fail "no runner type specified, check <runner> -> <type> element"
+    end
+
+    @runner =
+      case runner_type.to_s
+        when "local" then LocalRunner.new
+        else fail "invalid runner type '#{runner_type}' specified"
+      end
   end
 
-  def compile
-    source_dir = Dir.pwd
-    Dir.chdir(make_build_dir)
+  def run_tests
 
+    compiled_tests = []
+
+    source_dir = Dir.pwd
+    build_dir = make_build_dir
+
+    # compile test binaries
+    Dir.chdir(build_dir)
     for test in @tests
       compile_command = get_compile_command(source_dir, test)
       print "Compiling #{test}..."
@@ -44,10 +94,18 @@ class TestSet
 
       if $? == 0
         print "[ok]\n"
+        compiled_tests += [test]
       else
+        @failed_tests += 1
         print "error\n", res
       end
     end
+    Dir.chdir(source_dir)
+
+    # run test that were successfully compiled
+    @runner.run(build_dir, compiled_tests)
+    @passed_tests += @runner.passed
+    @failed_tests += @runner.failed
   end
 
   def make_build_dir
@@ -59,8 +117,15 @@ class TestSet
 
   def get_compile_command(source_dir, test)
     "make VPATH=#{source_dir} XDC=#{source_dir}/../xdc " +
-         "CFLAGS=\"-g -m32 -lgc\" DFLAGS=--march=#{@arch} " +
-         "-f #{source_dir}/Makefile check_#{test}"
+         "CFLAGS=\"-g -m32 -lgc\" " +
+         "DFLAGS=\"--march=#{@arch} -I#{source_dir}\" " +
+         "-f #{source_dir}/Makefile check_#{test} 2>&1"
+  end
+
+  def get_summary
+   "#{@tests.length} TEST SUITES RUN, " +
+   "#{@passed_tests} PASSED " +
+   "#{@failed_tests} FAILED"
   end
 end
 
@@ -68,9 +133,15 @@ end
 tests = File.new("tests.list").read.split
 
 # build and run tests
+test_sets = []
 for conf in Dir["*.conf"]
-  ts = TestSet.new(tests, conf)
-  ts.compile
+  target = TestTarget.new(tests, conf)
+  print "Running tests for '", target.name,  "'\n"
+  target.run_tests
+
+  test_sets += [target]
 end
 
+# print test results for each target
+test_sets.each { |t| print t.name, ": ", t.get_summary, "\n" }
 
