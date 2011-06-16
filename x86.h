d2d819a3cac871352b71f868578a48004f3730e2
Lennart Blanco <cokebuttle@gmail.com>
2011-06-16 17:34:05 +0200

    x86: removed dead function declarations

--- x86.h
+++ x86.h
@@ -23,21 +23,4 @@ typedef struct x86_comp_params_s
 void
 x86_init(arch_backend_t *backend);
 
-void
-x86_compile_expression(x86_comp_params_t *params,
-                       IrExpression *expression,
-                       sym_table_t *sym_table);
-
-int
-x86_get_expression_storage_size(IrExpression *expression);
-
-/**
- * Alligned an address/offset in 32-bit word boundary, 
- * by increasing it if needed.
- *
- * @return word alligned address
- */
-int
-x86_inc_to_word_boundary(int addr);
-
 #endif /* X86_INC_X */
