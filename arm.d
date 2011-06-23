b3f6d933b57444fcdb383fdd1d8639077ac8b1b4
Lennart Blanco <cokebuttle@gmail.com>
2011-06-23 07:52:34 +0200

    arm: added asserts flaging unimplemented features

--- arm.d
+++ arm.d
@@ -914,6 +914,7 @@ compile_set(File asmfile, iml_operation *op)
     assert(iml_operand_get_data_type(src) == iml_data_type._32b ||
            iml_operand_get_data_type(src) == iml_data_type.ptr,
            "only 32-bit set is implemented");
+    assert(offset == null, "set offset not implemented");
 
     /* make sure source operand is placed in a register */
     string src_reg = store_in_reg(asmfile, src, TEMP_REG1);
@@ -931,6 +932,8 @@ private void
 compile_get(File asmfile, iml_operation *op)
 {
     assert(iml_operation_get_opcode(op) == iml_opcode.get);
+    assert(iml_operation_get_operand(op, 2) == null,
+           "get offset not implemented");
 
     /* make sure address value is stored in the register */
     string addr_reg =
