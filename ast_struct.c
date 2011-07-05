da827909ef83e83b69f37355d228bf4fccae4b19
Lennart Blanco <cokebuttle@gmail.com>
2011-07-05 07:18:39 +0200

    removed commented out code

--- ast_struct.c
+++ ast_struct.c
@@ -2,16 +2,6 @@
 
 #include <assert.h>
 
-///*---------------------------------------------------------------------------*
-// *                  local functions forward declaration                      *
-// *---------------------------------------------------------------------------*/
-//
-//static void
-//ast_enum_do_print(AstNode *self, FILE *out, int indention);
-//
-//static void
-//ast_enum_class_init(gpointer klass, gpointer dummy);
-
 /*---------------------------------------------------------------------------*
  *                           exported functions                              *
  *---------------------------------------------------------------------------*/
@@ -71,39 +61,3 @@ ast_struct_get_members(AstStruct *self)
 
     return self->members;
 }
-
-///*---------------------------------------------------------------------------*
-// *                             local functions                               *
-// *---------------------------------------------------------------------------*/
-//
-//static void
-//ast_enum_do_print(AstNode *self, FILE *out, int indention)
-//{
-//    assert(AST_IS_ENUM(self));
-//    assert(out);
-//
-//    AstEnum *e = AST_ENUM(self);
-//    GSList *i;
-//
-//    fprintf(out, "  enum\n");
-//    fprintf(out, "    tag: %s\n",
-//            e->tag ? e->tag : "<anonymous>");
-//    fprintf(out,
-//            "    base type: %s\n"
-//            "    members:\n",
-//            e->base_type ? dt_data_type_get_string(e->base_type) :
-//                           "<unspecified>");
-//
-//    for (i = e->members; i != NULL; i = g_slist_next(i))
-//    {
-//        fprintf(out, "      ");
-//        ast_node_print(i->data, out, indention);
-//    }
-//}
-//
-//static void
-//ast_enum_class_init(gpointer klass, gpointer dummy)
-//{
-//    ((AstNodeClass *)klass)->do_print = ast_enum_do_print;
-//}
-
