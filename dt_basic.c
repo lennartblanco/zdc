0fc60b66e1fe56be97de20b631704f4e6ef54649
Lennart Blanco <cokebuttle@gmail.com>
2011-10-05 23:54:50 +0200

    Implemented is_impl_conv() method of DT_BASIC class.

--- dt_basic.c
+++ dt_basic.c
@@ -6,6 +6,32 @@
 #include <assert.h>
 
 /*---------------------------------------------------------------------------*
+ *                             static table                                  *
+ *---------------------------------------------------------------------------*/
+
+/**
+ * table of allowed implicit casts between basic types
+ *
+ * column index is the source type, row index is target type,
+ * if cell at impl_convs[target_type][source_type] == true, then implicit
+ * conversation betweem source_type and target_type is allowed
+ */
+static bool impl_convs[last_basic_type][last_basic_type] =
+{
+             /* void  char   bool   byte   ubyte  short  ushort int    uint */
+ /*  void  */ {false, false, false, false, false, false, false, false, false},
+ /*  char  */ {false, true,  true,  true,  true,  false, false, false, false},
+ /*  bool  */ {false, false, true,  false, false, false, false, false, false},
+ /*  byte  */ {false, true,  true,  true,  true,  false, false, false, false},
+ /* ubyte  */ {false, true,  true,  true,  true,  false, false, false, false},
+ /* short  */ {false, true,  true,  true,  true,  true,  true,  false, false},
+ /* ushort */ {false, true,  true,  true,  true,  true,  true,  false, false},
+ /*  int   */ {false, true,  true,  true,  true,  true,  true,  true,  true},
+ /*  uint  */ {false, true,  true,  true,  true,  true,  true,  true,  true}
+};
+
+
+/*---------------------------------------------------------------------------*
  *                  local functions forward declaration                      *
  *---------------------------------------------------------------------------*/
 
@@ -314,6 +340,22 @@ dt_basic_is_integral(DtDataType *self)
     return DT_BASIC(self)->data_type != void_type;
 }
 
+static bool
+dt_basic_is_impl_conv(DtDataType *self, DtDataType *type)
+{
+    assert(dt_is_basic(self));
+    assert(DT_IS_DATA_TYPE(type));
+
+    if (!dt_is_basic(type))
+    {
+        return false;
+    }
+
+    return
+        impl_convs[dt_basic_get_data_type(DT_BASIC(self))]
+                  [dt_basic_get_data_type(DT_BASIC(type))];
+}
+
 static void
 dt_basic_class_init(gpointer klass, gpointer dummy)
 {
@@ -323,6 +365,7 @@ dt_basic_class_init(gpointer klass, gpointer dummy)
     DT_DATA_TYPE_CLASS(klass)->get_init = dt_basic_get_init;
     DT_DATA_TYPE_CLASS(klass)->is_same = dt_basic_is_same;
     DT_DATA_TYPE_CLASS(klass)->is_integral = dt_basic_is_integral;
+    DT_DATA_TYPE_CLASS(klass)->is_impl_conv = dt_basic_is_impl_conv;
 }
 
 static bool
