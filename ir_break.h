3027287f3d55f2ec1ea39500629e5609b4f96507
Lennart Blanco <cokebuttle@gmail.com>
2012-04-24 07:52:03 +0200

    corrected IR_BREAK_GET_CLASS macro

--- ir_break.h
+++ ir_break.h
@@ -22,7 +22,7 @@
   (G_TYPE_CHECK_CLASS_TYPE ((klass), IR_TYPE_BREAK))
 
 #define IR_BREAK_GET_CLASS(obj) \
-  (G_TYPE_INSTANCE_GET_CLASS ((obj), IR_TYPE_BREAK, IrReturnClass))
+  (G_TYPE_INSTANCE_GET_CLASS ((obj), IR_TYPE_BREAK, IrBreakClass))
 
 typedef struct
 {
